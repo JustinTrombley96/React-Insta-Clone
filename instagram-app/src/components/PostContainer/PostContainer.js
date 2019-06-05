5f132e4f36c0e86ef0e534ff53821d98972f2788
Justin Trombley <jtrombley96@gmail.com>
2019-06-05 02:36:15 -0400

    Update PostContainer.js

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,49 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 import CommentSection from '../CommentSection/CommentSection';
 import PropTypes from 'prop-types'
 import './PostContainer.scss'
 
-export class PostContainer extends Component {
-  
-
+class PostContainer extends React.Component {
 	render() {
-        return (
-        <div className="post-container">
-
-          {this.props.filteredPosts.length === 0 ? this.props.data.map(post => {
-              return (
-                 <>
-                 <header>
-                     <img src={post.thumbnailUrl} alt="thumbnail"/>  
-                     <h3>{post.username}</h3>  
-                 </header>
-                 <img src={post.imageUrl} alt={post.id}/>
-                 <section className="likebar">
-                     {post.likes} likes
-                 </section>
-                 <CommentSection comments={post.comments} timestamp={post.timestamp} />
-                 </>
-              )
-          }): this.props.data.map(post => {
-            return (
-               <>
-               <header>
-                   <img src={post.thumbnailUrl} alt="thumbnail"/>  
-                   <h3>{post.username}</h3>  
-               </header>
-               <img src={post.imageUrl} alt={post.id}/>
-               <section className="likebar">
-                   {post.likes} likes
-               </section>
-               <CommentSection comments={post.comments} timestamp={post.timestamp} />
-               </>
-            ) }
-            )}
-         </div>
-        )
-        
-}
-}
+	  return (
+		<div className='post-container'>
+		  {this.props.filteredPosts.length === 0
+			? this.props.data.map(post => {
+				return (
+				  <>
+					<header>
+					  <img src={post.thumbnailUrl} alt='thumbnail' />
+					  <h3>{post.username}</h3>
+					</header>
+   
+					<img src={post.imageUrl} alt={post.id} />
+   
+					<section className='likebar'>{post.likes} likes</section>
+   
+					<CommentSection
+					  comments={post.comments}
+					  timestamp={post.timestamp}
+					/>
+				  </>
+				);
+			  })
+			: this.props.filteredPosts.map(post => {
+				return (
+				  <>
+					<header>
+					  <img src={post.thumbnailUrl} alt='thumbnail' />
+					  <h3>{post.username}</h3>
+					</header>
+   
+					<img src={post.imageUrl} alt={post.id} />
+   
+					<section className='likebar'>{post.likes} likes</section>
+   
+					<CommentSection
+					  comments={post.comments}
+					  timestamp={post.timestamp}
+					/>
+				  </>
+				);
+			  })}
+		</div>
+	  );
+	}
+   }
 
 
 
