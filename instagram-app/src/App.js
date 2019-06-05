6d2fc21bedabb3f63fcf9c4f20aa21967fcdf724
Justin Trombley <jtrombley96@gmail.com>
2019-06-04 21:15:48 -0400

    Update App.js

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -6,14 +6,40 @@ import PostContainer from './components/PostContainer/PostContainer';
 
 class App extends Component {
 	state = {
-		data : [],
+		data     : [],
+		comments : this.state.comments,
+		id       : '',
+		username : 'MyZarZar',
+		text     : '',
 	};
-
 	componentDidMount() {
 		this.setState({
 			data : dummyData,
 		});
 	}
+
+	handleChanges = event => {
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	};
+
+	addComment = (event, index) => {
+		event.preventDefault();
+
+		const newComment = {
+			id       : this.state.id,
+			username : this.state.username,
+			text     : this.state.text,
+		};
+
+		this.setState({
+			comments : [ ...this.state.comments, newComment ],
+			id       : '',
+			username : 'MyZarZar',
+			text     : this.state.text,
+		});
+	};
 
 	render() {
 		return (
